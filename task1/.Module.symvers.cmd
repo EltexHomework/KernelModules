0b24f345943788609b56ee9489bb2e9739106b8c
TheNorth322 <lol_danchik@bk.ru>
2024-10-09 18:02:53 +0700

    remove unused files

--- task1/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/thenorth/eltex/KernelModules/task1/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/thenorth/eltex/KernelModules/task1/Module.symvers -T /home/thenorth/eltex/KernelModules/task1/modules.order -i Module.symvers -e 
